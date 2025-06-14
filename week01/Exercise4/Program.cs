75cd33d5ec5984b67863d1019693957f2e3c565d
rmharden <har22079@byui.edu>
2025-06-14 17:31:06 -0400

    Not sure if it's right, but I put the using.system.collections.generic; under using system;

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
     }
 }
