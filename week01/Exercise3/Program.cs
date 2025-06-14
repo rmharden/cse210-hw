8fa0e6473d5554e57713183eec2a3cd797f65023
rmharden <har22079@byui.edu>
2025-06-14 17:08:16 -0400

    Completed parts 1 and 2.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //Part 1 and 2
+        //Console.Write("What is the magic number? ");
+        //int magicnumber = int.Parse(Console.ReadLine());
+
+        //Part 3
+
+        
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
