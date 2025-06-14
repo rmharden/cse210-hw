d50d874ceb31c428324f1179097ce9542767fda8
rmharden <har22079@byui.edu>
2025-06-14 17:42:38 -0400

    Began working through excercise 4.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,5 +7,29 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the Exercise4 Project.");
 
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter a number (0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sume is: {sum}");
+
     }
 }
