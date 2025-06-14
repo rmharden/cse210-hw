c9088e1068fe469075dfdf35980f818877e87ef2
rmharden <har22079@byui.edu>
2025-06-14 15:17:25 -0400

    Started the W01 Exercise 02.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradepercentage = Console.ReadLine();
+        
     }
 }
