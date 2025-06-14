27a3724461d22a20c762a1897348ff75cabcd987
rmharden <har22079@byui.edu>
2025-06-14 15:28:38 -0400

    Wrote the program for w01 exercise 2.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,12 +1,46 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
         Console.Write("What is your grade percentage? ");
         string gradepercentage = Console.ReadLine();
-        
+        int number = int.Parse(gradepercentage);
+
+        string lettergrade = "";
+
+        if (number >= 90)
+        {
+            lettergrade = "A";
+        }
+        if (number >= 80)
+        {
+            lettergrade = "B";
+        }
+        if (number >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (number >= 60)
+        {
+            lettergrade = "D";
+        }
+
+        Console.WriteLine($"Your grade is: {lettergrade}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass.");
+        }
+
     }
 }
