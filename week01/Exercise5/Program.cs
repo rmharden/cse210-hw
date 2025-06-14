07f375fa6805f923ab8013c4b5c55c253430cb6b
rmharden <har22079@byui.edu>
2025-06-14 19:37:32 -0400

    Started W01 exercise 05.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        Console.WriteLine("Welcome to the Program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your favorite number? ");
+        string favorite = Console.ReadLine();
+        int number = int.Parse(favorite);
+
+        Console.Write($"{name}, the square of your number is {number}.")
     }
 }
