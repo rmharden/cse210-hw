a80f1c086b4ee7eb678ec9f9094656f808559a04
rmharden <har22079@byui.edu>
2025-06-14 21:55:36 -0400

    Trying to fix mistakes.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,21 +5,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayProgramMessage();
 
+        string usersName = AskUsersName();
+        int usersFavoriteNo = AskUsersNumber();
+
+        int squaredNumber = SquareNumber(squaredNumber);
+
+        DisplayResult(usersName, squaredNumber);
+
+
+    }
+
+    static void DisplayProgramMessage()
+    {
         Console.WriteLine("Welcome to the Program!");
+    }
 
+    static string AskUsersName(string name)
+    {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
+    }
 
-        
+    static int AskUsersNumber()
+    {
 
         Console.Write("What is your favorite number? ");
         string favorite = Console.ReadLine();
         int number = int.Parse(favorite);
 
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
         int square = number * number;
 
+        return square;
+
+    }
+    static void DisplayResult(string name, int square)
+    {
         Console.Write($"{name}, the square of your number is {square}.");
     }
 }
