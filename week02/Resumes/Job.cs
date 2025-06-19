8c65063eed9c439ebaa3e51ca3a88d89b40c76a5
rmharden <har22079@byui.edu>
2025-06-19 19:26:10 -0400

    Adding a Display method to the Job.cs class.

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -3,5 +3,10 @@ public class Job
     public string _company;
     public string _jobTitle;
     public int _startYear;
-    public int _endYear; 
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine();
+    }
 }
