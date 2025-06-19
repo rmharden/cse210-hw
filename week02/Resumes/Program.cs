987e38e4119d08d8e424de42a2956e43d935abc2
rmharden <har22079@byui.edu>
2025-06-19 19:32:49 -0400

    Added the data for start year and end year to job1 in the Program.cs.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,6 +7,8 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._company = "Apple";
