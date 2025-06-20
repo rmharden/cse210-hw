dc32e1c9d3e23752459b1245a86b39114242e4ac
rmharden <har22079@byui.edu>
2025-06-20 12:09:05 -0400

    Added a comment.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -5,9 +5,11 @@ public class PromptGenerator
 {
     public List<string> prompts = new List<string>();
 
+    //I think this is a constructor. A internet search says
+    //that contructors are methods that are named the same
+    //as the class. I think I'm confused. :(
     public PromptGenerator()
     {
-        //I don't know why this isn't work yet.
         prompts.Add("Who was the most interesting person I interacted with today?");
         prompts.Add("What was the best part of my day?");
         prompts.Add("How did I see the hand of the Lord in my life today?");
