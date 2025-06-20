72cd5a1705635862cb9067b7003889c9bb6407a1
rmharden <har22079@byui.edu>
2025-06-20 12:27:50 -0400

    I don't know how to do the random prompt. I'm stuck.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    public List<string> prompts = new List<string>();
+    public string _prompts;
+    public List<string> prompt = new List<string>();
 
     //I think this is a constructor. A internet search says
     //that contructors are methods that are named the same
     //as the class. I think I'm confused. :(
     public PromptGenerator()
     {
-        prompts.Add("Who was the most interesting person I interacted with today?");
-        prompts.Add("What was the best part of my day?");
-        prompts.Add("How did I see the hand of the Lord in my life today?");
-        prompts.Add("What was the strongest emotion I felt today?");
-        prompts.Add("If I had one thing I could do over today, what would it be?");
+        prompt.Add("Who was the most interesting person I interacted with today?");
+        prompt.Add("What was the best part of my day?");
+        prompt.Add("How did I see the hand of the Lord in my life today?");
+        prompt.Add("What was the strongest emotion I felt today?");
+        prompt.Add("If I had one thing I could do over today, what would it be?");
     }
 
-    
+    public string GetRandomPrompt()
+    {
+        Random randomPrompt = new Random();
 
+    }
 }
