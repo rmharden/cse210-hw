bae358a43781c08b218889b143f9c615c41d04a6
rmharden <har22079@byui.edu>
2025-06-20 10:29:09 -0400

    Added a Display method.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -4,6 +4,10 @@ public class Entry
     public string _promptText;
     public string _entryText;
 
-    
+    public void Display()
+    {
+        //I'm not sure this is all that is displayed yet. 
+        Console.WriteLine(_promptText);
+    }
 
 }
