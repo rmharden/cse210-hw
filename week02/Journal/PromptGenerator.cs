d366ecb747f4a00e15be063b0ebda10957101bdb
rmharden <har22079@byui.edu>
2025-06-20 18:38:57 -0400

    Fixed a typo in a comment.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -4,7 +4,7 @@ public class PromptGenerator
 
     public string GetRandomPrompt()
     {
-        //This is going to return a empty string.
+        //This is going to return an empty string.
         //It helps to add this for now so I can
         //run this code without a problem.
         //This returns an empty string for now.
