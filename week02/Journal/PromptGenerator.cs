62c3f1659af4087c94c52a158a9aacf71ef3a3b6
rmharden <har22079@byui.edu>
2025-06-20 10:54:29 -0400

    Started the code for the PromptGenerator class.

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,10 @@
+//Added this because it was in the example for lists in our
+//Week 01 lesson. I don't understand what it does yet.
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> prompts = new List<string>();
+
+}
