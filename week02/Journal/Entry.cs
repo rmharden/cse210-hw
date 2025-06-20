ea5ae4ef0eb3e31bfff762653e1bb2499de271cf
rmharden <har22079@byui.edu>
2025-06-20 10:32:41 -0400

    Added more to the Display method.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -6,8 +6,11 @@ public class Entry
 
     public void Display()
     {
-        //I'm not sure this is all that is displayed yet. 
-        Console.WriteLine(_promptText);
+        //I'm not sure this is all that is displayed yet.
+        //I don't know if the Entry class is only for
+        //the menu option 2.
+        Console.WriteLine($"Date: {_date} - {_promptText}");
+        Console.WriteLine(_entryText);
     }
 
 }
