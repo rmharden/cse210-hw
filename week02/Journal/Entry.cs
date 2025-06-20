df6f91338ea19a0ea34b1963d0779d4d4f3aa21d
rmharden <har22079@byui.edu>
2025-06-20 18:48:46 -0400

    I think I added what will be displayed for the Entry class when the menu option 2. Display is chosen by the user in the Journal class.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,12 +1,13 @@
 public class Entry
 {
+    //This class is for the menu option: 2. Display.
     public string _date;
     public string _promptText;
     public string _entryText;
 
     public void Display()
     {
-        Console.WriteLine($"Date: {_date} - {_promptText}");
-        
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine(_entryText);
     }
 }
