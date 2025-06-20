e0eb71c7071134980d5cafcab6bbe29e5f09eb76
rmharden <har22079@byui.edu>
2025-06-20 18:31:43 -0400

    Added comments and the GetRandomPrompt method with the List attribute.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,4 +1,13 @@
 public class PromptGenerator
 {
-    
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        //This is going to return a empty string.
+        //It helps to add this for now so I can
+        //run this code without a problem.
+        //This returns an empty string for now.
+        return "";
+    }
 }
