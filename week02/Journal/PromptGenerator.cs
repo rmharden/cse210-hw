48ae88a5e40bdb644135ec120dce1e99e00bac90
rmharden <har22079@byui.edu>
2025-06-20 12:07:09 -0400

    Added what I think might be a constructor but I don't know if this is correct.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,15 +1,18 @@
-
+using System;
+using System.Collections.Generic;
 
 public class PromptGenerator
 {
     public List<string> prompts = new List<string>();
 
-    //I don't know why this isn't work yet.
-    prompts.Add("Who was the most interesting person I interacted with today?");
-    prompts.Add("What was the best part of my day?");
-    prompts.Add("How did I see the hand of the Lord in my life today?");
-    prompts.Add("What was the strongest emotion I felt today?");
-    prompts.Add("If I had one thing I could do over today, what would it be?");
-
+    public PromptGenerator()
+    {
+        //I don't know why this isn't work yet.
+        prompts.Add("Who was the most interesting person I interacted with today?");
+        prompts.Add("What was the best part of my day?");
+        prompts.Add("How did I see the hand of the Lord in my life today?");
+        prompts.Add("What was the strongest emotion I felt today?");
+        prompts.Add("If I had one thing I could do over today, what would it be?");
+    }
 
 }
