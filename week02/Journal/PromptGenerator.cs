de8c3300d0a94b528a072faff7b3fc363a300295
rmharden <har22079@byui.edu>
2025-06-20 11:00:06 -0400

    Adding prompt questions to the PromptGenerator class.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -5,6 +5,11 @@ using System.Collections.Generic;
 
 public class PromptGenerator
 {
-    public List<string> prompts = new List<string>();
+    List<string> prompts = new List<string>();
+
+    prompts.Add("Who was the most interesting person I interacted with today?");
+    prompts.Add("What was the best part of my day?");
+    prompts.Add("How did I see the hand of the Lord in my life today?");
+    prompts.Add()
 
 }
