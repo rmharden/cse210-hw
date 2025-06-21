def1876836fed19b2a0919bfdc86fc653878c1ed
rmharden <har22079@byui.edu>
2025-06-21 08:41:09 -0400

    The Program.cs file and the PromptGenerator.cs file has errors. Trying to fix the problem.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,28 +1,9 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //        Journal theJournal = new Journal();
-        //        Entry anEntry = new Entry();
-        //        anEntry.Display();
-
-        //        Console.WriteLine("Hello World! This is the Journal Project.");
-
-        List<string> prompts = new List<string>();
-        prompts.Add("Who was the most interesting person I interacted with today?");
-        prompts.Add("What was the best part of my day?");
-        prompts.Add("How did I see the hand of the Lord in my life today?");
-        prompts.Add("What was the strongest emotion I felt today?");
-        prompts.Add("If I had one thing I could do over today, what would it be?");
-
-        Console.WriteLine("Here are all the journal prompts:");
-        foreach (string prompt in prompts)
-        {
-            Console.WriteLine(prompt);
-        }
-
+        Console.WriteLine("Hello World.");
     }
 }
