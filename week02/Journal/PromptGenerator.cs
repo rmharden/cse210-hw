62f33244a2046c70484224d7b7d541e78cda7351
rmharden <har22079@byui.edu>
2025-06-21 08:36:05 -0400

    Fixing errors in the prompt generator.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,17 +1,32 @@
+using System;
+using System.Collections.Generic;
 public class PromptGenerator
 {
-    //public List<string> _prompts;
+    public List<string> _prompts;
 
+    List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
 
-    //public string GetRandomPrompt()
-    //{
-    //  Random randomPrompts = new Random();
-    //string promptText = randomPrompts.Next();
+    Console.WriteLine("Here are all the journal prompts:");
+    
+    foreach (string prompt in prompts)
+    {
+        Console.WriteLine(prompt);
+    }    
 
-    //This is going to return an empty string.
-    //It helps to add this for now so I can
-    //run this code without a problem.
-    //This returns an empty string for now.
-    //return "";
-    //}
+    public string GetRandomPrompt()
+    {
+
+        //This is going to return an empty string.
+        //It helps to add this for now so I can
+        //run this code without a problem.
+        //This returns an empty string for now.
+        //return "";
+    }
 }
