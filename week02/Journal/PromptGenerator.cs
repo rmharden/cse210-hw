38e229ee0c3dc396f1ab88febfef1e983c83b9a8
rmharden <har22079@byui.edu>
2025-06-22 00:10:06 -0400

    Moved my list out of my get random prompt.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -4,29 +4,19 @@ using System.Collections.Generic;
 public class PromptGenerator
 {
     public List<string> _prompts;
-/// <summary>
-///    List<string> prompts = new List<string>
-///    {
-///        "Who was the most interesting person I interacted with today?",
-///        "What was the best part of my day?",
-///        "How did I see the hand of the Lord in my life today?",
-///        "What was the strongest emotion I felt today?",
-///        "If I had one thing I could do over today, what would it be?"
-///    };
-/// </summary>
+
+    List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+   };
+
     public string GetRandomPrompt()
     {
         Random rnd = new();
-        List<string> prompts = new List<string>
-        {
-            "Who was the most interesting person I interacted with today?",
-            "What was the best part of my day?",
-            "How did I see the hand of the Lord in my life today?",
-            "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?"
-        };
-
-
         //Temporarily returns an empty string for now.
         return "";
     }
