10bc8f4535d8649b1ff393ee66f8c6f31a1ccff3
rmharden <har22079@byui.edu>
2025-06-26 10:58:40 -0400

    Began writing the attributes and created a class for Fraction.CS.

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,22 @@
+using System;
+
+/*
+W03 Learning Activity: Encapsulation
+
+Followed the class diagram for the setup.
+*/
+
+//Creates the Fraction class
+public class Fraction
+{
+    //This is an atrribute for the numerator. 
+    private int _top;
+    //This is an attribute for the denominator.
+    private int _bottom;
+
+    //Does this make a ?
+    public Fraction()
+    {
+
+    }
+}
