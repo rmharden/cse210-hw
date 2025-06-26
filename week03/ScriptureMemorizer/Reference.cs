c2b00b6c431703e7b1da98c548657c560e6de84a
rmharden <har22079@byui.edu>
2025-06-26 19:53:07 -0400

    Attempted to write the Reference class. There are errors I need to learn how to solve.

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,25 +1,35 @@
+using System.Collections.Concurrent;
+
 public class Reference
 {
     private string _book;
     private int _chapter;
     private int _verse;
+    private int _startVerse;
     private int _endVerse;
 
-    public Reference(string book, int chapter, int verse)
+    public Reference()
     {
-        Reference reference = new Reference();
-        reference._book = "John";
-        reference._chapter = 3;
-        reference._verse = 16;
+        _book = "";
+        _chapter = 0;
+        _verse = 0;
+        _startVerse = 0;
+        _endVerse = 0;
+    }
+
+    public Reference(string book, int chapter, int _verse);
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
     }
 
     public Reference(string book, int chapter, int startVerse, int endVerse)
     {
-        Reference reference = new Reference();
-        reference._book = "Proverbs";
-        reference._chapter = 3;
-        reference._verse = 5;
-        reference._endVerse = 6;
+        _book = book;
+        _chapter = chapter;
+        _startVerse = startVerse;
+    _endVerse = endVerse;    
     }
 
     //This is a getter.
