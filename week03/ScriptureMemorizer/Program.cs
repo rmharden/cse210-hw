8c89d0159ee5314f053b08fe35e96d1498153c2a
rmharden <har22079@byui.edu>
2025-06-26 17:42:20 -0400

    I added the displayed message about pressing or typing 'quit' to end the program to the main Program.cs - Not sure it goes here.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
-        
+        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
     }
 }
--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -7,7 +7,7 @@ public class Reference
 
     public Reference(string book, int chapter, int verse)
     {
-
+        
     }
 
     public Reference(string book, int chapter, int startVerse, int endVerse)
