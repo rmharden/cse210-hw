af43f6acd0d5f65e5bd8cfea2eb2b75f93865cb2
rmharden <har22079@byui.edu>
2025-06-26 15:10:03 -0400

    W03 Learning Activity: Encapsulation Completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -78,20 +78,21 @@ class Program
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
 
-        Fraction f2 = new Fraction();
+        //Step 6, no. 3: adding 5
+        Fraction f2 = new Fraction(5);
         f2.GetFractionString();
         f2.GetDecimalValue();
         Console.WriteLine(f2.GetFractionString());
         Console.WriteLine(f2.GetDecimalValue());
 
-        // Step. 6, no. 1.
+        // Step 6: no. 1:
         Fraction f3 = new Fraction(3, 4);
         f3.GetFractionString();
         f3.GetDecimalValue();
         Console.WriteLine(f3.GetFractionString());
         Console.WriteLine(f3.GetDecimalValue());
 
-        //Step 6, no. 3.
+        //Step 6: no. 3:
         Fraction f4 = new Fraction(1, 3);
         f4.GetFractionString();
         f4.GetDecimalValue();
