c124c8fad08ba799ceb33ff6909f80f003f0f1ea
rmharden <har22079@byui.edu>
2025-06-29 18:08:54 -0400

    Practicing the CSE 210 CSV Reading and Writing Demo in Sandbox.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,11 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        /*I deleted my code I was testing so I can use
-        this for other things later.*/
-        Console.WriteLine("Hello World! THis is the Sandbox Project.");
+        Person p1 = new Person();
+        p1._firstName = "Mary";
+        p1._lastName = "Smith";
+        p1._age = 25;
+
+        Person p2 = new Person();
+        p2._firstName = "John";
+        p2._lastName = "Watkins";
+        p2._age = 30;
+
+        List<Person> people = new List<Person>();
+        people.Add(p1);
+        people.Add(p2);
+
+        foreach (Person p in people)
+        {
+            Console.WriteLine(p._firstName);
+        }
+
     }
 }
--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -7,17 +7,18 @@ public class Word
 
     public Word(string text)
     {
-
+        _text = text;
+        _isHidden = false;
     }
 
     public void Hide()
     {
-
+        _isHidden = true;
     }
 
     public void Show()
     {
-
+        _isHidden = false;
     }
 
     public bool IsHidden()
@@ -29,6 +30,6 @@ public class Word
     public string GetDisplayText()
     {
         //Returns an empty string.
-        return "";
+        return _text;
     }
 }
