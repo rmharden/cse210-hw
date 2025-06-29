f839cf52656065d0dd4702da5eed5d2bc417117a
rmharden <har22079@byui.edu>
2025-06-29 19:14:56 -0400

    Finished the CVS Reading and Writing: An Example Walkthrough  demo.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,15 +7,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         Person p1 = new Person();
-        p1._firstName = "Mary";
-        p1._lastName = "Smith";
-        p1._age = 25;
+        p1._firstName = "June";
+        p1._lastName = "Logan";
+        p1._age = 32;
 
         Person p2 = new Person();
-        p2._firstName = "John";
-        p2._lastName = "Watkins";
-        p2._age = 30;
+        p2._firstName = "Webster";
+        p2._lastName = "McMullen";
+        p2._age = 51;
 
         List<Person> people = new List<Person>();
         people.Add(p1);
@@ -26,6 +27,13 @@ class Program
             Console.WriteLine(p._firstName);
         }
         SaveToFile(people);
+        */
+
+        List<Person> newPeople = ReadFromFile();
+        foreach (Person p in newPeople)
+        {
+            Console.WriteLine(p._lastName);
+        }
     }
     public static void SaveToFile(List<Person> people)
     {
@@ -42,7 +50,30 @@ class Program
     }
     public static List<Person> ReadFromFile()
     {
-        
+        Console.WriteLine("Reading list from file...");
+        List<Person> people = new List<Person>();
         string filename = "people.txt";
+
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            //Console.WriteLine(line);
+            // line will have something like this: "Mary~~Smith~~25
+            //John~~Watkins~~30"
+            string[] parts = line.Split("~~");
+
+            // parts[0] - Mary
+            // parts[1] - Smith
+            // parts[2] - 25
+
+            Person newPerson = new Person();
+            newPerson._firstName = parts[0];
+            newPerson._lastName = parts[1];
+            newPerson._age = int.Parse(parts[2]);
+
+            people.Add(newPerson);
+        }
+        return people;
     }
 }
