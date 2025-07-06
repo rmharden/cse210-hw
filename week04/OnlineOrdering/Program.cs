aa0676140101bb1b68bd636b54b42a5513709eba
rmharden <har22079@byui.edu>
2025-07-05 22:20:06 -0400

    created a second product

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -9,11 +9,22 @@ class Program
         order1._quantity = 5;
         order1._name = "lamps";
 
+        Product order2 = new Product();
+        order2._price = 250;
+        order2._quantity = 2;
+        order2._name = "chairs";
+
+        /*
         Console.WriteLine(order1._price);
         Console.WriteLine(order1._quantity);
         Console.WriteLine(order1._name);
-        double orderCost = order1.GetTotal();
-        Console.WriteLine(orderCost);
+        */
+
+        double firstOrderCost = order1.GetTotal();
+        Console.WriteLine(firstOrderCost);
+
+        double secondOrderCost = order1.GetTotal();
+        Console.WriteLine(secondOrderCost);
 
     }
 }
