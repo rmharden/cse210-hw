62f8961ec99ebee0740928e07028985d5879a8d8
rmharden <har22079@byui.edu>
2025-07-07 16:13:24 -0400

    added a constructor for Assignment that receives and sets the member variables. Added a return output for the GetSummary method.

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -3,8 +3,18 @@ public class Assignment
     private string _studentName;
     private string _topic;
 
+
+    // This is a constructor that receives a student name
+    // and the topic. It sets the member variables.
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    
     public string GetSummary()
     {
-        return "";
+        return _studentName + "-" + _topic;
     }
 }
