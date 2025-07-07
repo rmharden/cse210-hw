596fd9716b5c9eb2ed26b46c556bb0aeedd2374b
rmharden <har22079@byui.edu>
2025-07-07 15:27:50 -0400

    Writing the MathAssignment class.

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,39 @@
+
+public class MathAssignment
+{
+    private string _studentName;
+    private string _topic;
+    private string _textbookSection;
+    private string _problems;
+
+    // This is the constructor that stores each value:
+    public MathAssignment()
+    {
+
+    }
+
+    public string GetSummary()
+    {
+        return "";
+    }
+
+    public string GetHomeworkList()
+    {
+        return "";
+    }
+}
+
+/*
+
+Class: MathAssignment
+
+A Math assignment needs to store:
+- the student's name
+- the topic
+- the textbook section
+- the problems from that section
+
+It has a constructor
+- requires a value for each of the items that it stores
+
+*/
