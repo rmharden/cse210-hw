ac02a91db6e6e6687b5dfba97345162cf08eb5b1
rmharden <har22079@byui.edu>
2025-07-07 17:11:05 -0400

    I deleted somelines from the program code.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -15,8 +15,6 @@ class Program
 
         WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(a3.GetSummary());
-        Console.WriteLine(a3.GetWritingInformation());
-
-        
+        Console.WriteLine(a3.GetWritingInformation());    
     }
 }
