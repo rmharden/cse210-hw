201343cc035e01727ee13d05b71d55e2674023f0
rmharden <har22079@byui.edu>
2025-07-07 19:10:34 -0400

    added more of the stubs to the classes.

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,7 +1,26 @@
 public class Activiy()
 {
-    public void Run()
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public void DisplayStartingMessage()
     {
 
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(in seconds)
+    {
+        return 0;
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        return 0;
     }
 }
--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,4 +1,28 @@
 public class ReflectingActivity()
 {
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+
+    }
     
 }
