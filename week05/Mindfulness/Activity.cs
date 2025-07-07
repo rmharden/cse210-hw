bb72c0a81d26a4c801b80448c0797c49af6abe22
rmharden <har22079@byui.edu>
2025-07-07 18:54:08 -0400

    created stubs for the activity base.

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,25 @@
+public class Activiy()
+{
+    private string startingMessage;
+    private string endingMessage;
+
+    public string DisplayStartMessage()
+    {
+        return "";
+    }
+
+    public string DisplayEndMessage()
+    {
+        return "";
+    }
+
+    public void SecondsSpinner()
+    {
+
+    }
+
+    public void CountdownTimmer()
+    {
+
+    }
+}
