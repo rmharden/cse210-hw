3a4281d6a4d7009bdc7dcd8853e52d97f4ddcb52
rmharden <har22079@byui.edu>
2025-07-07 17:08:16 -0400

    my WritingAssignment class file was missing the .cs extension, which is why the main program couldn't find it. Not I think it might work.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -16,5 +16,7 @@ class Program
         WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(a3.GetSummary());
         Console.WriteLine(a3.GetWritingInformation());
+
+        
     }
 }
