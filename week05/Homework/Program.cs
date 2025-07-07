61a4f1d678f10177f6265ca06a74403e327294ba
rmharden <har22079@byui.edu>
2025-07-07 16:17:05 -0400

    created a simple assignment in Program.cs in the Main method to test the GetSummary method.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Step 3:
+        // Using the Sample Output as info:
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
     }
 }
