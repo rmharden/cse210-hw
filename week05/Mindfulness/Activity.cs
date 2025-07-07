b507b5c53d6b6094d3334574eecdbb0bcb511066
rmharden <har22079@byui.edu>
2025-07-07 19:04:56 -0400

    Wrote more stubs in ListingActivity

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,24 +1,6 @@
 public class Activiy()
 {
-    private string startingMessage;
-    private string endingMessage;
-
-    public string DisplayStartMessage()
-    {
-        return "";
-    }
-
-    public string DisplayEndMessage()
-    {
-        return "";
-    }
-
-    public void SecondsSpinner()
-    {
-
-    }
-
-    public void CountdownTimmer()
+    public void Run()
     {
 
     }
--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,4 +1,20 @@
 public class ListingActivity()
 {
-    
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetListFromUser()
+    {
+        List<string> UserList = new List<string>();
+        return "";
+    }
+
+    public void Run()
+    {
+        
+    }
+
+
 }
