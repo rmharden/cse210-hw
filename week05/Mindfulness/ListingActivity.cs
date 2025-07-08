aefa3bd8eb24caf1c27b251378c0b5d58353ce20
rmharden <har22079@byui.edu>
2025-07-08 16:18:15 -0400

    Fixed the code in ListingActitity.

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,8 +1,25 @@
-public class ListingActivity
+public class ListingActivity : Activity
 {
     public ListingActivity()
     {
+        Console.Clear();
+        Console.WriteLine("\nWelcome to the Listing Activity.");
+        Console.WriteLine("\nThis activity will help you reflect on the good things \nin your life by having you list as \nmany things as you can in a certain area.");
 
+        Console.Write("\nHow long, in seconds, would you like for your session? ");
+        string userInput = Console.ReadLine();
+
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        Console.Read();
+
+        Console.WriteLine("Breathe in...");
+        Console.WriteLine("Now breathe out...");
+
+        Console.WriteLine("\nWell done!!");
+
+        Console.WriteLine($"\nYou have completed another {userInput} seconds of the Breathing Activity.\n");
+        Console.Read();
     }
 }
     /*
