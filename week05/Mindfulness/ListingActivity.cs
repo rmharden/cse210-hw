3806af85606995044653ca04a2fc98e382dd805f
rmharden <har22079@byui.edu>
2025-07-08 19:52:26 -0400

    duplicated the random prompt generator and made one for the  ListingActivity.cs.

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -25,20 +25,21 @@ public class ListingActivity : Activity
         Console.WriteLine($"\nYou have completed another {userInput} seconds of the Listing Activity.\n");
         Console.Read();
     }
-}
-    /*
-        private List<string> userEntries = new List<string>();
-        public string GetListFromUser()
-        {
-            Console.WriteLine("Type an entry or type 'quit' to finish");
-            string userEntry = Console.ReadLine();
-            userEntries.Add(userEntry);
-
-            foreach (string e in userEntries)
+    public string GetRandomPrompt()
+    {
+        Random displayPrompt = new Random();
+        List<string> _prompts = new List<string>()
             {
-                Console.WriteLine(e);
-            }
-            return GetListFromUser();
-        }
-    */
+                "> 1. Who are people that you appreciate?",
+                "> 2. What are personal strengths of yours?",
+                "> 3. Who are people that you have helped this week?",
+                "> 4. When have you felt the Holy Ghost this month?",
+                "> 5. Who are some of your personal heroes?",
+                "> 6. What is your favorite thing about this experience?"
+            };
+        int pIndex = displayPrompt.Next(_prompts.Count);
+        Console.WriteLine($"--- {_prompts[pIndex]} ---");
+        return _prompts[pIndex];
+    }
+}
    
