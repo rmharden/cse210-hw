3a5bcb9b0359cbefb4e3afa9a42c12a64388e6e2
rmharden <har22079@byui.edu>
2025-07-09 17:37:20 -0400

    Adding code to the BreathingActivity. The spinner is going to go in Activity.cs but I am testing it in BreathingActivity.

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -19,17 +19,38 @@ public class Activity
 
     public void DisplayStartingMessage()
     {
+        Console.WriteLine($"\nWelcome to the {_name} activity.");
 
+        Console.WriteLine($"\n{_description}");
+
+        Console.Write("How long, in seconds, would you like your session? ");
     }
 
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine($"Well done!!");
     }
 
     public void ShowSpinner(int seconds)
     {
+        Console.WriteLine("Get Ready...");
 
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("\\");
+
+        foreach (string s in animationStrings)
+        {
+            Console.Write(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+        
     }
 
     public void ShowCountDown(int seconds)
--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -8,24 +8,74 @@ public class BreathingActivity : Activity
         Console.WriteLine("\nThis activity will help you relax by walking you through breathing in and out slowly. \nClear your mind and focus on your breathing.");
 
         Console.Write("\nHow long, in seconds, would you like for your session? ");
-        string userInput = Console.ReadLine();
+        string _duration = Console.ReadLine();
 
         Console.Clear();
         Console.WriteLine("Get ready...");
+        //ShowSpinner();
+        Spinner();
         Console.Read();
 
-        Console.WriteLine("Breathe in...");
-        Console.WriteLine("Now breathe out...");
+        Run();
 
         Console.WriteLine("\nWell done!!");
+        Spinner();
 
-        Console.WriteLine($"\nYou have completed another {userInput} seconds of the Breathing Activity.\n");
+        Console.WriteLine($"\nYou have completed another {_duration} seconds of the Breathing Activity.\n");
         Console.Read();
     }
 
     public void Run()
     {
+        Console.Write("Breathe in...");
+        for (int i = 5; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
 
+        Console.Write("Now breathe out...");
+        for (int i = 6; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+    public void Spinner()
+    {
+        // This is here temorarily
+
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("\\");
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(5);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+
+            string s = animationStrings[i];
+            Console.Write(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= animationStrings.Count)
+            {
+                i = 0;
+            }
+        }
     }
 }
 
