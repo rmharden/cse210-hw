39d5ac381fb65e7db6f635b9a75af6bff8d78857
rmharden <har22079@byui.edu>
2025-07-10 18:13:45 -0400

    Wrote a basic entry class, but I am not sure this is what we're supposed to do. I think we are to bring this in from the list right? so how do I do that?

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -5,7 +5,7 @@ public class Entry
     public string _entryText;
     public void Display()
     {
-        Console.WriteLine($"");
-        Console.WriteLine($"");
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"{_entryText}");
     }    
 }
