b6ed351e6a323f31b898750ae61d6b451768a648
rmharden <har22079@byui.edu>
2025-07-10 18:14:15 -0400

    Added new lines

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -5,7 +5,7 @@ public class Entry
     public string _entryText;
     public void Display()
     {
-        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
-        Console.WriteLine($"{_entryText}");
+        Console.WriteLine($"\nDate: {_date} - Prompt: {_promptText}");
+        Console.WriteLine($"{_entryText}\n");
     }    
 }
