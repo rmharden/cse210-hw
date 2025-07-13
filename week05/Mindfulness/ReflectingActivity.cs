5771667410250ad79ad6f0ecdd33b714407778f6
rmharden <har22079@byui.edu>
2025-07-13 19:49:40 -0400

    Wrote out the questions.

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -44,7 +44,15 @@ public class ReflectingActivity
         Random displayQuestions = new Random();
         List<string> _questions = new List<string>()
         { 
-            
+            "> 1. Why was this experience meaningful to you?",
+            "> 2. Have you ever done anything like this before?",
+            "> 3. How did you get started?",
+            "> 4. How did you feel when it was complete?",
+            "> 5. What made this time different than other times when you were not as successful?",
+            "> 6. What is your favorite thing about this experience?",
+            "> 7. What could you learn from this experience that applies to other situations?",
+            "> 8. What did you learn about yourself through this experience?",
+            "> 9. How can you keep this experience in mind in the future?"
         };
         int qIndex = displayQuestions.Next(_questions.Count);
         Console.WriteLine($"--- {_questions[qIndex]} ---");
