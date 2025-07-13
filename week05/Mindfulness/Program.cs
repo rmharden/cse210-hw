b2d16d4d720475dadb11c279f9b82d8c772a9a18
rmharden <har22079@byui.edu>
2025-07-13 19:02:59 -0400

    Re-writing the menu

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,6 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        string response = "";
+        while (response != "4")
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start the breathing activity");
+            Console.WriteLine("2. Start the reflecting activity");
+            Console.WriteLine("3. Start the listing activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            response = Console.ReadLine();
+            if (response == "1")
+            {
+
+            }
+            else if (response == "2")
+            {
+
+            }
+
+        }
     }
 }
