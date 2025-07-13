e30071e9ce65f485ce30ca1a5bdf6321c2b61cf1
rmharden <har22079@byui.edu>
2025-07-12 21:38:25 -0400

    I'm working on getting everything that is in the breathing program that should be in the activity base out of the cs file.

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,35 +1,16 @@
-
 using System;
-public class BreathingActivity //: Activity
+public class BreathingActivity : Activity
 {
+
+    // From our class diagram, this class only has BreathingActivity() and Run() : void.
     public BreathingActivity()
     {
-        Console.Clear();
-        Console.WriteLine("\nWelcome to the Breathing Activity.");
-        Console.WriteLine("\nThis activity will help you relax by walking you through breathing in and out slowly. \nClear your mind and focus on your breathing.");
 
-        Console.Write("\nHow long, in seconds, would you like for your session? ");
-        string _duration = Console.ReadLine();
-
-        Console.Clear();
-        Console.WriteLine("Get ready...");
-        //ShowSpinner();
-        Spinner();
-        Console.Read();
-
-        Run();
-
-        Console.WriteLine("\nWell done!!");
-        Spinner();
-
-        Console.WriteLine($"\nYou have completed another {_duration} seconds of the Breathing Activity.\n");
-        Console.Read();
     }
 
     public void Run()
     {
-        // Week 01 Curriculum about the int.Parse() function
-        // Week 05 Video Demo on animations
+        // I used Week 01 Curriculum about the int.Parse() function and the Week 05 Video Demo on animations
         Console.WriteLine("\nHow long, in seconds, would you like your session? ");
         string inputString = Console.ReadLine();
         int duration = int.Parse(inputString);
@@ -54,42 +35,7 @@ public class BreathingActivity //: Activity
                 Thread.Sleep(1000);
                 Console.Write("\b \b");
             }
-        }
-        Console.WriteLine("\nWell done!\n");
-    }
-    public void Spinner()
-
-    {
-        // This is here temorarily
-
-        List<string> animationStrings = new List<string>();
-        animationStrings.Add("|");
-        animationStrings.Add("/");
-        animationStrings.Add("-");
-        animationStrings.Add("\\");
-        animationStrings.Add("|");
-        animationStrings.Add("/");
-        animationStrings.Add("\\");
-
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = startTime.AddSeconds(5);
-
-        int i = 0;
-
-        while (DateTime.Now < endTime)
-        {
-
-            string s = animationStrings[i];
-            Console.Write(s);
-            Thread.Sleep(1000);
-            Console.Write("\b \b");
-
-            i++;
-
-            if (i >= animationStrings.Count)
-            {
-                i = 0;
-            }
+            Console.WriteLine();
         }
     }
 }
