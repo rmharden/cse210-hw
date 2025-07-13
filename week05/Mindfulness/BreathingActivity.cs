dc7a382cdff8408b7fabc73dfefda6e17683bcbf
rmharden <har22079@byui.edu>
2025-07-13 19:26:36 -0400

    wrote more of the breathing activity.

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,14 +1,33 @@
+using System.Runtime.InteropServices.Marshalling;
+
 public class BreathingActivity
 {
     public BreathingActivity()
     {
         Console.Clear();
         Console.WriteLine("\nWelcome to the Breathing Activity.");
-        Console.WriteLine("\nThis activity will help you relax by walking you through breathing in and out slowly.Clear your mind and focus on your breathing.");
-        Console.WriteLine("\nHow long, in seconds, would you like for your session?");
-        string _duration = Console.ReadLine();
+        Console.WriteLine("\nby walking you through breathing in and out slowly.Clear your mind and focus on your breathing.");
 
-        Console.Clear();
-        Console.WriteLine("Get ready...");
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(10);
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("\nBreathe in...");
+            for (int bi = 5; bi > 0; bi--)
+            {
+                Console.Write(bi);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+            }
+            Console.WriteLine();
+            Console.Write("Breathe out");
+            for (int bo = 5; bo > 0; bo--)
+            {
+                Console.Write(bo);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");                
+            }
+        }
+
     }
 }
