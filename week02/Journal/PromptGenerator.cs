733cc6b86b45c937f24d038dd966bbba3fff8be8
rmharden <har22079@byui.edu>
2025-07-14 11:42:19 -0400

    Wrote the prompt generator.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,6 +1,17 @@
-
+public class PromptGenerator
+{
+    public string GetRandomPrompt()
+    {
+        Random displayPrompt = new Random();
+        List<string> _prompts = new List<string>()
+        {
             "Who was the most interesting person I interacted with today?",
             "What was the best part of my day?",
             "How did I see the hand of the Lord in my life today?",
             "What was the strongest emotion I felt today?",
             "If I had one thing I could do over today, what would it be?"
+        };
+        int i = displayPrompt.Next(_prompts.Count);
+        return _prompts[i];
+    }
+}
