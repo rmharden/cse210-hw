c95ee0a2484d125ab3708b837d16c7dec5001dc9
rmharden <har22079@byui.edu>
2025-07-13 21:28:42 -0400

    Began writing the activity base class.

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,26 @@
+using System;
+class Activity
+{
+    public Activity()
+    {
+
+    }
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+    public int ShowSpinner()
+    {
+        return 0;
+    }
+
+    public int ShowCountDown()
+    {
+        return 0;
+    }
+}
