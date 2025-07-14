f24e07636a7c8143054304279855f770ee41730f
rmharden <har22079@byui.edu>
2025-07-13 21:34:13 -0400

    added a spinner without a given time.

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -16,6 +16,22 @@ class Activity
     }
     public int ShowSpinner()
     {
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+
+        foreach (string s in animationStrings)
+        {
+            Console.WriteLine(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
         return 0;
     }
 
