f599dabf668656b24aeb696b68e88e38fed8de1c
rmharden <har22079@byui.edu>
2025-07-14 11:25:05 -0400

    Re-writing the prompt generator

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,25 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Runtime.ConstrainedExecution;
-
-// Supplies random prompts whenever needed.
-
 public class PromptGenerator
 {
-    public String GetRandomPrompt()
+    List<string> _prompts = new List<string>()
     {
-        List<string> _prompts = new List<string>()
-        {
-            "1. Who was the most interesting person I interacted with today?",
-            "2. What was the best part of my day?",
-            "3. How did I see the hand of the Lord in my life today?",
-            "4. What was the strongest emotion I felt today?",
-            "5. If I had one thing I could do over today, what would it be?"
-        };
-        Random random = new Random();
-        int pIndex = random.Next(_prompts.Count);
-        string promptText = _prompts[pIndex];
-        // Console.WriteLine(promptText); // This was creating a duplicate prompt.
-        return promptText;
+        "1. Who was the most interesting person I interacted with today?",
+        "2. What was the best part of my day?",
+        "3. How did I see the hand of the Lord in my life today?",
+        "4. What was the strongest emotion I felt today?",
+        "5. If I had one thing I could do over today, what would it be?"
+    };
+    public string GetRandomPrompt()
+    {
+        
+        Random displayPrompt = new Random();
+
+        return prompt;
     }
+
 }
