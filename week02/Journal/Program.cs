2ddb5b1978e752da1a2b69ac47c33e17ada4f6e8
rmharden <har22079@byui.edu>
2025-07-14 12:22:44 -0400

    Wrote a menu.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -8,8 +8,41 @@ class Program
         while (response != "5")
         {
             Console.Clear();
-            PromptGenerator randomPrompt = new PromptGenerator();
-            randomPrompt.GetRandomPrompt();
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+
+            response = Console.ReadLine();
+            if (response == "1")
+            {
+                PromptGenerator randomPrompt = new PromptGenerator();
+                randomPrompt.GetRandomPrompt();
+            }
+            else if (response == "2")
+            {
+
+            }
+            else if (response == "3")
+            {
+
+            }
+            else if (response == "4")
+            {
+
+            }
+            else if (response == "5")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid number\n");
+            }
         }
     }
 }
