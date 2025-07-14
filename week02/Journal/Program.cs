30e76153fdb5d98a1701cd883a004869c934e6ca
rmharden <har22079@byui.edu>
2025-07-14 11:29:46 -0400

    calling the prompt generator in the main program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,5 +4,5 @@ using System;
 
 class Program
 {
-    
+    PromptGenerator displayPrompt = new PromptGenerator();
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,19 +1,21 @@
 public class PromptGenerator
 {
-    List<string> _prompts = new List<string>()
-    {
-        "1. Who was the most interesting person I interacted with today?",
-        "2. What was the best part of my day?",
-        "3. How did I see the hand of the Lord in my life today?",
-        "4. What was the strongest emotion I felt today?",
-        "5. If I had one thing I could do over today, what would it be?"
-    };
+ 
     public string GetRandomPrompt()
     {
         
-        Random displayPrompt = new Random();
-
-        return prompt;
+        Random prompt = new Random();
+        List<string> _prompts = new List<string>()
+        {
+            "1. Who was the most interesting person I interacted with today?",
+            "2. What was the best part of my day?",
+            "3. How did I see the hand of the Lord in my life today?",
+            "4. What was the strongest emotion I felt today?",
+            "5. If I had one thing I could do over today, what would it be?"
+        };
+        int i = prompt.Next(_prompts.Count);
+        Console.WriteLine(_prompts[i]);
+        return _prompts[i];
     }
 
 }
