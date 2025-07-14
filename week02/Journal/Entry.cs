39e66dcf3053efd99b95de14e3f3d171f95e21fd
rmharden <har22079@byui.edu>
2025-07-14 11:36:01 -0400

    Re-writing the journal program

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,13 +1,1 @@
-public class Entry
-{
-    //This class is for the menu option: 2. Display.
-    public string _date;
-    public string _promptText;
-    public string _entryText;
-
-    public void Display()
-    {
-        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
-        Console.WriteLine(_entryText);
-    }
-}
+// Re-writing the Journal program based on what I learned as I studied for the Mindfulness program.
