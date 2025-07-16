f027c212676f90d084410e7b0d11bb44bb9fdbf1
rmharden <har22079@byui.edu>
2025-07-15 20:51:20 -0400

    Writing the Square class.

--- week06/Shapes/Square.cs
+++ week06/Shapes/Square.cs
@@ -1,4 +1,16 @@
 public class Square : Shapes
 {
+    private double _side;
 
+    public Square()
+    {
+
+    }
+
+
+    // Not sure about this. It populated this for me:
+    public override double GetArea()
+    {
+        return base.GetArea();
+    }
 }
