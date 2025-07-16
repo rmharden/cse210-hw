2419e347d4c3eea85d956b4826aea0656c161b21
rmharden <har22079@byui.edu>
2025-07-15 20:38:04 -0400

    starting the stubs of the homework

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,20 @@
+public class Shapes
+{
+    string _color;
+
+    public string GetColor()
+    {
+        return "";
+    }
+
+    public void SetColor(string color)
+    {
+        
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+
+}
