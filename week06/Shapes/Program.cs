9b315564c570c51f46ef349a437e145c489b3c9c
rmharden <har22079@byui.edu>
2025-07-15 21:03:32 -0400

    Trying to test the square class.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        // is this a instance of Square?
+        Square s1 = new Square("green", 5);
+        Console.WriteLine(s1.GetColor());
+        Console.WriteLine(s1.GetArea());
+
+
     }
 }
