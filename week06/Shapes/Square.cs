1ac963a60dc7a3432fe4b9aaafff30402b710671
rmharden <har22079@byui.edu>
2025-07-15 21:00:18 -0400

    Finishing the Square class according to the directions.

--- week06/Shapes/Square.cs
+++ week06/Shapes/Square.cs
@@ -1,16 +1,20 @@
+using System.Drawing;
+
 public class Square : Shapes
 {
     private double _side;
 
-    public Square()
+    // error says the base says it doesn't take a constructor...
+    public Square(string color, double side) : base(color)
     {
-        
+        _side = side;
     }
 
 
     // Not sure about this. It populated this for me:
     public override double GetArea()
     {
-        return base.GetArea();
+        // return base.GetArea();
+        return 0;
     }
 }
