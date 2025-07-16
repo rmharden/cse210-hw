b15603ccf40e0b0daf4101d839a9a9e4e4a6a3c4
rmharden <har22079@byui.edu>
2025-07-15 20:42:02 -0400

    Writing the shape base class.

--- week06/Shapes/Shape.cs
+++ week06/Shapes/Shape.cs
@@ -1,15 +1,18 @@
 public class Shapes
 {
+    // this is a member variable
     string _color;
 
+    // This is the getter
     public string GetColor()
     {
         return "";
     }
 
+    // This is a setter
     public void SetColor(string color)
     {
-        
+        _color = color;
     }
 
     public virtual double GetArea()
