4cbb5fb234caefeceafdbd8c80691ff6cae3c120
rmharden <har22079@byui.edu>
2025-07-16 15:15:00 -0400

    Created the member variables for rectangle and changed the get area for rectangle

--- week06/Shapes/Rectangle.cs
+++ week06/Shapes/Rectangle.cs
@@ -1,15 +1,17 @@
 public class Rectangle : Shape
 {
-    private double _side;
+    private double _length;
+    private double _width;
 
-    public Rectangle(string color, double side) : base (color)
+    public Rectangle(string color, double length, double width) : base(color)
     {
-        _side = side;
+        _length = length;
+        _width = width;
     }
 
 
     public override double GetArea()
     {
-        return _side * _side;
+        return _length * _width;
     }
 }
