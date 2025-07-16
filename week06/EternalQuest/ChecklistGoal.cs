91cc4fe7c749155742ea3c386aa539f19567fde7
rmharden <har22079@byui.edu>
2025-07-16 18:34:28 -0400

    Typing out the stubs

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -1,4 +1,40 @@
-public class CheckListGoal
+using System.Runtime;
+
+public class CheckListGoal : Goal
 {
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public CheckListGoal(string name, string description, string points, int target, int bonus) : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+    }
+
+    /// unsure what to do here
+    // public static void RecordEvent()
+    // {
+
+    // }
+
+
+    // Also, not sure what to do here
+    // public bool IsComplete()
+    //{
+
+    //}
+
+    // Also, not sure what to do here
+    // public string GetDetailsString()
+    // {
+
+    // }
+
+
+    // Also, not sure what to do here
+    // public string GetStringRepresentation()
+    // {
     
+    // }
 }
