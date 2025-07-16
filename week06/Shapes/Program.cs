a5ae150c7818711b1cfb6db143ea9d549cad90e1
rmharden <har22079@byui.edu>
2025-07-16 15:12:50 -0400

    added a new line in the program class

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine("Hello World! This is the Shapes Project.\n");
 
         // is this a instance of Square?
         Square s1 = new Square("green", 5);
+        Console.WriteLine("This is a square:");
         Console.WriteLine(s1.GetColor());
         Console.WriteLine(s1.GetArea());
 
