661ec0caac6164db093206077fb853ba84239657
rmharden <har22079@byui.edu>
2025-07-16 18:15:56 -0400

    writing stubs in each class.

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,4 +1,37 @@
+using System.Security.Cryptography.X509Certificates;
+
 public class Goal
 {
-    
+    // I believe this is the base class
+
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool IsComplete()
+    {
+        return true; //this is here temporarily
+    }
+
+    public string GetDetailsString()
+    {
+        return "";
+    }
+
+    public string GetStringRepresentation()
+    {
+        return "";
+    }
 }
--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -1,4 +1,51 @@
 public class GoalManager
 {
-    
+    private List<Goal> _goals;
+    private int _store;
+
+    public GoalManager()
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void DisplayPlayerInfo()
+    {
+
+    }
+
+    public void ListGoalNames()
+    {
+
+    }
+
+    public void ListGoalDetails()
+    {
+        
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public void SaveGoals()
+    {
+
+    }
+
+
+    public void LoadGoals()
+    {
+
+    }
 }
