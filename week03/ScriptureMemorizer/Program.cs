53cd2820123b450e6fe1f46610015b090238e780
rmharden <har22079@byui.edu>
2025-07-18 17:53:41 -0400

    took off code I was trying.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -9,7 +9,7 @@ class Program
         // Console.WriteLine();
 
         Reference r1 = new Reference();
-        Scripture s1 = new Scripture(r1, "").Scripture;
+        Scripture s1 = new Scripture(r1, "");
 
         Reference r2 = new Reference("John", 3, 16);
         Scripture s2 = new Scripture(r2, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
