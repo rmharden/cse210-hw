4d9d3f7987fa0d1930959eae0b26114a236c25e4
rmharden <har22079@byui.edu>
2025-07-18 09:40:52 -0400

    wrote stubs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Console.WriteLine("Hello Scripture Memorizer World!");
     }
 }
 
---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,17 @@
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
