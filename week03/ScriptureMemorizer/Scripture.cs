dc5c4ec22c2381dc0603768738d781eae976bd10
rmharden <har22079@byui.edu>
2025-07-18 17:50:15 -0400

    Changed IsCompletelyHidden to false, added a loop to add words to the list... need to figure out how to keep the list a Word instead of String type.

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -2,11 +2,19 @@ public class Scripture
 {
     private Reference _reference;
 
-    private List<Word> _words;
+    //private List<Word> _words; // I need to make the type Word but I don't know how to add the objects to the list when they become an array.
+    private List<string> _words = new List<string>();
 
     private Scripture(Reference reference, string text)
     {
-        
+        _reference = reference; //This I am not sure... Do I call Reference here? or is it adding the info, because display text never works from here.
+
+        string[] words = text.Split(" ");
+        foreach (string word in words)
+        {
+            _words.AddRange(words);
+            Console.WriteLine(word[0]);
+        }
     }
 
     public void HideRandomWords(int numberToHide)
@@ -17,11 +25,13 @@ public class Scripture
     public string GetDisplayText()
     {
         //Returns an empty string.
-        return "";
+        //return"";
+
+        return _reference + " ";
     }
 
     public bool IsCompletelyHidden()
     {
-        return true;
+        return false;
     }
 }
