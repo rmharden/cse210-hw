021d5b7b929a2e6eac045835eeab42a400519bc3
rmharden <har22079@byui.edu>
2025-07-18 09:26:10 -0400

    Wrote the scriptures in comments.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,6 +5,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
     }
 }
+
+
+/*
+
+John 3:16
+
+For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.
+
+*/
+
+
+/*
+
+Proverbs 3:5-6
+
+5 Trust in the Lord with all thine heart; and lean not unto thine own understanding.
+
+6 In all thy ways acknowledge him, and he shall direct thy paths.
+
+*/
