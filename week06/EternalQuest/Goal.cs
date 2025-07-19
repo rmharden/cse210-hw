b00d4d1ef2a9933424a8efc4ab3dadcd71c2f327
rmharden <har22079@byui.edu>
2025-07-19 19:09:24 -0400

    wrote stubs in the goal class and added a contructor and initilialized variables.

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,4 +1,37 @@
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
 public class Goal
 {
-    
+    // Attributes (private or protected according to the video)
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public int RecordEvent()
+    {
+        return 0;
+    }
+
+    public bool IsComplete()
+    {
+        return false;
+    }
+
+    public string GetDetailsString()
+    {
+        return "";
+    }
+
+    public string GetRepresentation()
+    {
+        return "";
+    }
 }
