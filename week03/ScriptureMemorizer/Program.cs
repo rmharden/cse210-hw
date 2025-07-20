3631c986c95c44cde403b81a46d369fb72a86bd5
rmharden <har22079@byui.edu>
2025-07-20 12:39:52 -0400

    deleted everything in the classes.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,8 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference r1 = new Reference("John", 3, 16);
-        Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        // Reference r1 = new Reference("John", 3, 16);
+        // Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
 
         // Reference r2 = new Reference("Proverbs", 3, 5, 6);
         // Scripture s2= new Scripture(r2, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
