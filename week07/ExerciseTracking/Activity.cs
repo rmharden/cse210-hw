7306bee694ee0a2e1b5647ecabb5f789581588d1
rmharden <har22079@byui.edu>
2025-07-21 11:02:51 -0400

    Wrote some of the abstract methods in the base class.

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,8 +1,13 @@
-public class Activity
+public abstract class Activity
 {
     // This is the parent/base class
     public Activity()
     {
 
     }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+    public abstract string GetSummary();
 }
