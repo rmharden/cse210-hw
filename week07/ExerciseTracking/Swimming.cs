42b0df390cb35b1d1721e1afe44e5d763c200ce7
rmharden <har22079@byui.edu>
2025-07-21 18:18:45 -0400

    changed the summary back to miles.

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -35,7 +35,7 @@ public class Swmming : Activity
         string date = base.GetDate();
         decimal minutes = base.GetMinutes();
         string name = base.GetName();
-        return $"{date} {name} ({minutes} min)- Distance: {GetCalcDistance()} meters, Speed: {GetSpeed()} mph, Pace: {GetPace()} min per meter";
+        return $"{date} {name} ({minutes} min)- Distance: {GetCalcDistance()} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} min per mile";
         // Output example:
         // 03 Nov 2022 Swimming (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
     }
