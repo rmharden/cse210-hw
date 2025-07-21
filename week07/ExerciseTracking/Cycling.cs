46679f27f9fdeafce4b7e6d52d9072e58d1984ee
rmharden <har22079@byui.edu>
2025-07-21 13:25:10 -0400

    wrote code for calculating the running methods

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -2,37 +2,32 @@ public class Cycling
 {
     private string _date;
     private string _name;
-    private int _time;
-    private int _distance;
-    private decimal _speed;
-    private int _pace;
-    public Cycling(string date, string name, int time, int distance, decimal speed, int pace)
+    private double _minutes;
+    private double _distance;
+    private double _speed;
+    private double _pace;
+    public Cycling(string date, string name, double minutes, double distance)
     {
         _date = date;
         _name = name;
-        _time = time;
+        _minutes = minutes;
         _distance = distance;
-        _speed = speed;
-        _pace = pace;
     }
-    public int GetDistance()
+    public double GetSpeed()
     {
-        // Established in the main program?
-        return _distance;
-    }
-    public decimal GetSpeed()
-    {
+
         return _speed;
     }
-    public int GetPace()
+    public double GetPace()
     {
+
         return _pace;
     }
     public  string GetSummary()
     {
-        return $"{_date} {_name} ({_time} min)- Distance {_distance} miles, Speed {_speed} mph, Pace: {_pace} min per mile";
+        return $"{_date} {_name} ({_minutes} min)- Distance {_distance} miles, Speed {_speed} mph, Pace: {_pace} min per mile";
         // Output example:
-        // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
+        // 03 Nov 2022 Cycling (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
     
     }
 }
