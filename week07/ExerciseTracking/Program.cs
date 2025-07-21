502eac13b05d91e5c1b926f2949b42df8c11cf9c
rmharden <har22079@byui.edu>
2025-07-21 12:42:18 -0400

    Called the swimming class in the main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+
+        Swmming s1 = new Swmming(20, dateText, "Swimming", 30);
+        Console.WriteLine($"{s1}");
     }
 }
--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -10,16 +10,12 @@ public class Swmming
     private double _pace;
     private double _laps;
     private double _poolLength;
-    public Swmming(double poolLength, double laps, string date, string name, double time, double distance, double speed, double pace)
+    public Swmming(double laps, string date, string name, double time)
     {
-        _poolLength = poolLength;
-        _laps = laps;
+         _laps = laps;
         _date = date;
         _name = name;
         _time = time;
-        _distance = distance;
-        _speed = speed;
-        _pace = pace;
     }
     public double GetDistance()
     {
@@ -27,22 +23,22 @@ public class Swmming
         double _stepOne = _laps * _poolLength;
         double _stepTwo = 1000.00 * 0.52;
         _distance = _stepOne / _stepTwo;
-        // Established in the main program?
         return _distance;
     }
     public double GetSpeed()
     {
+        _speed = GetDistance() / _time;
         return _speed;
     }
     public double GetPace()
     {
+        _pace = _time / GetDistance();
         return _pace;
     }
     public  string GetSummary()
     {
         return $"{_date} {_name} ({_time} min)- Distance {_distance} miles, Speed {_speed} mph, Pace: {_pace} min per mile";
         // Output example:
-        // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
-    
+        // 03 Nov 2022 Swimming (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
     }
 }
