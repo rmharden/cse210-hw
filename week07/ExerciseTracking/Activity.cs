8d30ace044f9c38b11345d8b451f1dbafbda0145
rmharden <har22079@byui.edu>
2025-07-21 11:50:31 -0400

    Copied code from running to the other activities as a template.

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,10 +1,41 @@
 public abstract class Activity
 {
+    private string _date;
+    private string _name;
+    private int _time;
+    private int _distance;
+    private decimal _speed;
+    private int _pace;
     // This is the parent/base class
-    public Activity()
+    public Activity(string date, string name, int time, int distance, decimal speed, int pace)
     {
-
+        _date = date;
+        _name = name;
+        _time = time;
+        _distance = distance;
+        _speed = speed;
+        _pace = pace;
     }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public void SetDate(string date)
+    {
+        _date = date;
+    }
+
+        public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    } 
 
     public abstract double GetDistance();
     public abstract double GetSpeed();
--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,11 +1,37 @@
 public class Swimming
 {
-    public Swimming()
+    private string _date;
+    private string _name;
+    private int _time;
+    private int _distance;
+    private decimal _speed;
+    private int _pace;
+    public Swimming(string date, string name, int time, int distance, decimal speed, int pace)
     {
-
+        _date = date;
+        _name = name;
+        _time = time;
+        _distance = distance;
+        _speed = speed;
+        _pace = pace;
     }
-    public abstract double GetDistance();
-    public abstract double GetSpeed();
-    public abstract double GetPace();
-    public abstract string GetSummary();
+    public int GetDistance()
+    {
+        return 0;
+    }
+    public int GetSpeed()
+    {
+        return 0;
+    }
+    public int GetPace()
+    {
+        return 0;
+    }
+    public  string GetSummary()
+    {
+        return $"{_date} {_name} ({_time} min)- Distance {_distance} miles, Speed {_speed} mph, Pace: {_pace} min per mile";
+        // Output example:
+        // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
+    
+    }
 }
