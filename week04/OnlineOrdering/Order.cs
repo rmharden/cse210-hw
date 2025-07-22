9af9196531445f8e8a67d96f75b705e0d5baf725
rmharden <har22079@byui.edu>
2025-07-22 18:30:50 -0400

    Removed my list from order. Trying to understand how to work with object lists.

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -1,20 +1,18 @@
 public class Order
 {
-    private List<Order> _products = new List<Order>();
+    private List<Order> _product;
     private Customer _customer;
 
-    public Order(Customer customer, Order products)
+
+    public Order(Customer customer, List<Product> product)
     {
         _customer = customer;
+
     }
     public Customer GetCustomer()
     {
         return _customer;
     }
-    // public Customer SetCustomer(Customer customer)
-    // {
-    //     _customer = customer;
-    // }
 
     public void AddProduct()
     {
