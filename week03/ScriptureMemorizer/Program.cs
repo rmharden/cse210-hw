eeeda8b556d6e0e704ee3df5d70db107e7b5504c
rmharden <har22079@byui.edu>
2025-07-24 10:31:36 -0400

    Trying to practice - vs code is not allowing me to have "stubs" and will not run or debug unless all member variables are assigned, which means I can't test anything... why?

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,111 +1,125 @@
 using System;
-using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void main(string[] args)
     {
-        // This displays even thought I don't have it displaying... It must mean that the getdisplay at the end of this class runs automatically... do I have a console.writeline somewhere?
-        Reference r1 = new Reference("John", 3, 16);
-        Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
-
-        string userResponse = "";
-
-        while (userResponse.ToLower() != "quit")
-        {
-            //Console.Clear();
-            List<Word> _words = new List<Word>();
-            string _scripture = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
-            string[] s = _scripture.Split(" ");
-            // why does this specifically print the actual text of each item???
-            foreach (string word in s)
-            {
-                _words.Add(new Word(word));
-                // This shows the words
-                //Console.Write($"{word} ");
-            }
-            Console.WriteLine(_words.Count);
-
-            // foreach (Word w in _words)
-            // {
-            //     Console.WriteLine(w);
-            // }
-            for (int i = 0; i < _words.Count; i++)
-            {
-                Console.Write(_words[i]);
-            }
+        //Console.WriteLine("This is a test.");
+        Word w = new Word("truth");
+        Console.WriteLine(w.GetDisplayText());
 
 
-
-            // this also displays the object "Word" and not the word
-                // Word firstWord = _words[0]; Console.Write(firstWord);
-                // Console.Write(_words[1]);
-                // This prints numbers/ the item index in a list
-                // for (int i = 0; i < _words.Count; i++)
-                // {
-                //     Console.Write($"{i} ");
-                // }
-                // https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/iterators
-                // This also displays the object type for every item, not the item:
-                // foreach (Word w in _words)
-                // {
-                //     Console.Write(w.ToString() + " ");
-                // }
-
-
-
-
-                // Found something about "new":
-                // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.add?view=net-9.0
-
-                // Random r = new Random();
-                // int wordIndex = r.Next(_words.Count);
-                // Word randomWord = _words[wordIndex];
-                // Console.WriteLine(randomWord);
-                Console.WriteLine("\nPress enter to continue or type 'quit' to finish:\n");
-            userResponse = Console.ReadLine();
-        }     
     }
 }
+
 // using System;
+// using System.Collections.Generic;
 
 // class Program
 // {
 //     static void Main(string[] args)
 //     {
+//         // This displays even thought I don't have it displaying... It must mean that the getdisplay at the end of this class runs automatically... do I have a console.writeline somewhere?
 //         Reference r1 = new Reference("John", 3, 16);
 //         Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
-
-//         // Reference r2 = new Reference("Proverbs", 3, 5, 6);
-//         // Scripture s2= new Scripture(r2, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
-
-//         // // I am testing the word class
-//         // Word w = new Word("loved");
-//         // {
-//         //     Console.WriteLine(w.GetDisplayText());
-//         // }
-
 
 //         string userResponse = "";
 
 //         while (userResponse.ToLower() != "quit")
 //         {
 //             //Console.Clear();
+//             List<Word> _words = new List<Word>();
+//             string _scripture = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+//             string[] s = _scripture.Split(" ");
+//             // why does this specifically print the actual text of each item???
+//             foreach (string word in s)
+//             {
+//                 _words.Add(new Word(word));
+//                 // This shows the words
+//                 //Console.Write($"{word} ");
+//             }
+//             Console.WriteLine(_words.Count);
 
-//             //Console.WriteLine(r1.GetDisplayText());
-//             // Console.WriteLine(s1.GetDisplayText());
-   
-//             Console.WriteLine("\nPress enter to continue or type 'quit' to finish:\n");
+//             // foreach (Word w in _words)
+//             // {
+//             //     Console.WriteLine(w);
+//             // }
+//             for (int i = 0; i < _words.Count; i++)
+//             {
+//                 Console.Write(_words[i]);
+//             }
+
+
+
+//             // this also displays the object "Word" and not the word
+//                 // Word firstWord = _words[0]; Console.Write(firstWord);
+//                 // Console.Write(_words[1]);
+//                 // This prints numbers/ the item index in a list
+//                 // for (int i = 0; i < _words.Count; i++)
+//                 // {
+//                 //     Console.Write($"{i} ");
+//                 // }
+//                 // https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/iterators
+//                 // This also displays the object type for every item, not the item:
+//                 // foreach (Word w in _words)
+//                 // {
+//                 //     Console.Write(w.ToString() + " ");
+//                 // }
+
+
+
+
+//                 // Found something about "new":
+//                 // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.add?view=net-9.0
+
+//                 // Random r = new Random();
+//                 // int wordIndex = r.Next(_words.Count);
+//                 // Word randomWord = _words[wordIndex];
+//                 // Console.WriteLine(randomWord);
+//                 Console.WriteLine("\nPress enter to continue or type 'quit' to finish:\n");
 //             userResponse = Console.ReadLine();
-//         }
+//         }     
 //     }
 // }
+// // using System;
+
+// // class Program
+// // {
+// //     static void Main(string[] args)
+// //     {
+// //         Reference r1 = new Reference("John", 3, 16);
+// //         Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+
+// //         // Reference r2 = new Reference("Proverbs", 3, 5, 6);
+// //         // Scripture s2= new Scripture(r2, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+
+// //         // // I am testing the word class
+// //         // Word w = new Word("loved");
+// //         // {
+// //         //     Console.WriteLine(w.GetDisplayText());
+// //         // }
 
 
-// // Watched video:
-// // BYU - Idaho Videos CSE 210: Week 3 Assignment 
-// // from Chad Macbeth May, 7th, 2024
-// // https://video.byui.edu/media/t/1_sv3gxgzs/259941952
+// //         string userResponse = "";
 
-// // I found this about being able to read the Enter key from a user:
-// // https://learn.microsoft.com/en-us/dotnet/api/system.console.readkey?view=net-9.0
+// //         while (userResponse.ToLower() != "quit")
+// //         {
+// //             //Console.Clear();
+
+// //             //Console.WriteLine(r1.GetDisplayText());
+// //             // Console.WriteLine(s1.GetDisplayText());
+   
+// //             Console.WriteLine("\nPress enter to continue or type 'quit' to finish:\n");
+// //             userResponse = Console.ReadLine();
+// //         }
+// //     }
+// // }
+
+
+// // // Watched video:
+// // // BYU - Idaho Videos CSE 210: Week 3 Assignment 
+// // // from Chad Macbeth May, 7th, 2024
+// // // https://video.byui.edu/media/t/1_sv3gxgzs/259941952
+
+// // // I found this about being able to read the Enter key from a user:
+// // // https://learn.microsoft.com/en-us/dotnet/api/system.console.readkey?view=net-9.0
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,54 +1,83 @@
 public class Scripture
 {
-    private Reference _reference;
     private List<Word> _words;
-    private string _text;
-    public Scripture()
-    {
-        _reference = Reference._book; // I don't know what to put here.
-        _text = "";
-    }
+    private Reference _reference;
     public Scripture(Reference reference, string text)
     {
-        _reference = reference;
-        _words = new List<Word>(); // I keep having trouble with this. It tells me it's not assigned. I don't know why.
 
-        foreach (string t in text.Split(" "))
-        {
-            Word w = new Word(t);
-            _words.Add(w);
-            // Console.Write($"{t} "); //test
-        }
-
-        HideRandomWords(6);
-        // Console.WriteLine(GetDisplayText());
     }
-
     public void HideRandomWords(int numberToHide)
     {
+        List<Word> w = new List<Word>();
         Random r = new Random();
-        int wordIndex = r.Next(_words.Count);
-        Word randomWord = _words[wordIndex];
-       // Console.WriteLine(randomWord);
+
+
+
+
+    }
+    public string GetDisplayText()
+    {
+        return "";
+    }
+    public bool IsCompletelyHidden()
+    {
+        // I think this is an if statement.
+        return false;
+    }
+}
+
+
+// public class Scripture
+// {
+//     private Reference _reference;
+//     private List<Word> _words;
+//     private string _text;
+//     public Scripture()
+//     {
+//         _reference = Reference; // I don't know what to put here.
+//         _text = "";
+//     }
+//     public Scripture(Reference reference, string text)
+//     {
+//         _reference = reference;
+//         _words = new List<Word>(); // I keep having trouble with this. It tells me it's not assigned. I don't know why.
+
+//         foreach (string t in text.Split(" "))
+//         {
+//             Word w = new Word(t);
+//             _words.Add(w);
+//             // Console.Write($"{t} "); //test
+//         }
+
+//         HideRandomWords(6);
+//         // Console.WriteLine(GetDisplayText());
+//     }
+
+//     public void HideRandomWords(int numberToHide)
+//     {
+//         Random r = new Random();
+//         int wordIndex = r.Next(_words.Count);
+//         Word randomWord = _words[wordIndex];
+//        // Console.WriteLine(randomWord);
 
         
 
 
-        // Set the state of a randomly selected group of words to be hidden (video at 12:20 of 16:27)
-        // Need to find a set of visible words (video at 12:20 of 16:27) The video says that "C# has a lot of "cool functions" like IsHidden, that will tell you if it returns false, it is visible." so we must be using IsHidden here somehow.
-        // "Lots of ways we can do this, think about loops, think about if-statements, make sure you call that IsVisible function, etc." (video at 12:50 of 16:27
-        // "You might want to start one out to select a random set of words to hide and you don't care if the words are already hidden yet, <- a good place to begin, and then try to go in there and say, I just want to hide visible ones." (video at 13:08 of 16:27)
-        // "To hide the words, use the Hide function" (video at 13:30 of 16:27) When you do that, and you use GetDisplayText, Bro. Macbeth said it works.
-    }
+//         // Set the state of a randomly selected group of words to be hidden (video at 12:20 of 16:27)
+//         // Need to find a set of visible words (video at 12:20 of 16:27) The video says that "C# has a lot of "cool functions" like IsHidden, that will tell you if it returns false, it is visible." so we must be using IsHidden here somehow.
+//         // "Lots of ways we can do this, think about loops, think about if-statements, make sure you call that IsVisible function, etc." (video at 12:50 of 16:27
+//         // "You might want to start one out to select a random set of words to hide and you don't care if the words are already hidden yet, <- a good place to begin, and then try to go in there and say, I just want to hide visible ones." (video at 13:08 of 16:27)
+//         // "To hide the words, use the Hide function" (video at 13:30 of 16:27) When you do that, and you use GetDisplayText, Bro. Macbeth said it works.
+//     }
 
-    // public string GetDisplayText()
-    // {
-    //     // foreach (Word w in _words)
-    //     // {
-    //     // return "\n" + _reference.GetDisplayText() + " " + w.GetDisplayText() + " ";
-    //     // }
-    //     //return "\n" + _reference.GetDisplayText();
-    //     // return "";
-    //     return "\n" + _reference.GetDisplayText();
-    // }
-}
+//     // public string GetDisplayText()
+//     // {
+//     //     // foreach (Word w in _words)
+//     //     // {
+//     //     // return "\n" + _reference.GetDisplayText() + " " + w.GetDisplayText() + " ";
+//     //     // }
+//     //     //return "\n" + _reference.GetDisplayText();
+//     //     // return "";
+//     //     return "\n" + _reference.GetDisplayText();
+//     // }
+// }
